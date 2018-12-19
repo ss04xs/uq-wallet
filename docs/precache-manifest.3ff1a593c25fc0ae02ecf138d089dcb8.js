db7e3e1f0043bcf8279dbe79600c67b92656ac22
ShutoDev <dscx1490@gmail.com>
2018-12-20 01:15:45 +0900

    build

---
+++ docs/precache-manifest.3ff1a593c25fc0ae02ecf138d089dcb8.js
@@ -0,0 +1,159 @@
+self.__precacheManifest = [
+  {
+    "revision": "527eff645d8ead16666afb6931041279",
+    "url": "./static/font/fonts/open-iconic.otf"
+  },
+  {
+    "url": "./static/js/vendor.d35f3d7f151777164594.js"
+  },
+  {
+    "revision": "0ab639b24252200167eb88ed22d4c9dc",
+    "url": "./static/manifest.json"
+  },
+  {
+    "revision": "015807e815facbf594eb90cb998a340f",
+    "url": "./static/font/css/open-iconic.styl"
+  },
+  {
+    "url": "./static/js/manifest.3ad1d5771e9b13dbdad2.js"
+  },
+  {
+    "revision": "3cf97837524dd7445e9d1462e3c4afe2",
+    "url": "./static/font/fonts/open-iconic.woff"
+  },
+  {
+    "revision": "f22d501a35a87d9f21701cb031f6ea17",
+    "url": "./static/img/icons/safari-pinned-tab.svg"
+  },
+  {
+    "url": "./static/js/app.b0ab1df17b3ee65b5e85.js"
+  },
+  {
+    "revision": "eca2c26759a009a4a9361151efe99e74",
+    "url": "./static/font/fonts/open-iconic.svg"
+  },
+  {
+    "revision": "f154378dc2d7b86fdf3eadb379c0b055",
+    "url": "./static/font/css/open-iconic-bootstrap.min.css"
+  },
+  {
+    "revision": "a7129ee4a07796cd6e5ffc746d10e2f6",
+    "url": "./static/font/css/open-iconic-bootstrap.styl"
+  },
+  {
+    "revision": "d4fb397f10a20e5c233952e119167584",
+    "url": "./static/font/css/open-iconic-foundation.css"
+  },
+  {
+    "revision": "6c5adbf3a290679e225c8038312dc8ca",
+    "url": "./static/font/css/open-iconic-foundation.less"
+  },
+  {
+    "revision": "a5a94ae52a1b53b8989a1a6d3c6b0632",
+    "url": "./static/font/css/open-iconic.css"
+  },
+  {
+    "revision": "728920d43b654e5393c0808fe2652e41",
+    "url": "./static/font/css/open-iconic-foundation.styl"
+  },
+  {
+    "revision": "4440f42c8268f52155501ff9a250207a",
+    "url": "./static/font/css/open-iconic-foundation.scss"
+  },
+  {
+    "revision": "e8f317794e512df9168490ceeebeb707",
+    "url": "./static/font/css/open-iconic.less"
+  },
+  {
+    "revision": "f22ea2269c0ab6e94915f7689b46682b",
+    "url": "./static/font/css/open-iconic.min.css"
+  },
+  {
+    "revision": "ceff54d1ccb3fb586931618988952332",
+    "url": "./static/font/css/open-iconic.scss"
+  },
+  {
+    "revision": "47151e87c5a8666791a91007de4962da",
+    "url": "./static/font/fonts/open-iconic.eot"
+  },
+  {
+    "revision": "058a3335d15a3eb84e7ae3707ba09620",
+    "url": "./static/img/icons/mstile-150x150.png"
+  },
+  {
+    "revision": "b89032a4a5a1879f30ba05a13947f26f",
+    "url": "./static/img/icons/msapplication-icon-144x144.png"
+  },
+  {
+    "revision": "e687678103a7352ac95ed394292efb30",
+    "url": "./static/font/css/open-iconic-bootstrap.scss"
+  },
+  {
+    "revision": "93cc7aa654ae36b0828716f5ec3159cd",
+    "url": "./static/font/fonts/open-iconic.ttf"
+  },
+  {
+    "revision": "500cbf8f41e456b4ae3c1980ae676bcd",
+    "url": "./static/font/css/open-iconic-foundation.min.css"
+  },
+  {
+    "revision": "f130a0b70e386170cf6f011c0ca8c4f4",
+    "url": "./static/img/icons/android-chrome-192x192.png"
+  },
+  {
+    "revision": "0ff1bc4d14e5c9abcacba7c600d97814",
+    "url": "./static/img/icons/android-chrome-512x512.png"
+  },
+  {
+    "revision": "936d6e411cabd71f0e627011c3f18fe2",
+    "url": "./static/img/icons/apple-touch-icon-120x120.png"
+  },
+  {
+    "revision": "1a034e64d80905128113e5272a5ab95e",
+    "url": "./static/img/icons/apple-touch-icon-152x152.png"
+  },
+  {
+    "revision": "c43cd371a49ee4ca17ab3a60e72bdd51",
+    "url": "./static/img/icons/apple-touch-icon-180x180.png"
+  },
+  {
+    "revision": "9a2b5c0f19de617685b7b5b42464e7db",
+    "url": "./static/img/icons/apple-touch-icon-60x60.png"
+  },
+  {
+    "revision": "af28d69d59284dd202aa55e57227b11b",
+    "url": "./static/img/icons/apple-touch-icon-76x76.png"
+  },
+  {
+    "revision": "66830ea6be8e7e94fb55df9f7b778f2e",
+    "url": "./static/img/icons/apple-touch-icon.png"
+  },
+  {
+    "revision": "4bb1a55479d61843b89a2fdafa7849b3",
+    "url": "./static/img/icons/favicon-16x16.png"
+  },
+  {
+    "revision": "7a2fa8fc2aff558ca7b7e6d5dddb5315",
+    "url": "./static/img/icons/favicon.ico"
+  },
+  {
+    "revision": "98b614336d9a12cb3f7bedb001da6fca",
+    "url": "./static/img/icons/favicon-32x32.png"
+  },
+  {
+    "revision": "94f069124ec015fc6eef36ca50442a78",
+    "url": "./static/font/css/open-iconic-bootstrap.less"
+  },
+  {
+    "revision": "22065f9aa20bb574c199fa49b1936a02",
+    "url": "./static/font/css/open-iconic-bootstrap.css"
+  },
+  {
+    "revision": "b0ab1df17b3ee65b5e85",
+    "url": "./static/css/app.770734cb8e0c7a04f4afbce9c898b5db.css"
+  },
+  {
+    "revision": "edd390f7689d3c867ef4a3b4fe7c036a",
+    "url": "./index.html"
+  }
+];
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.46ff469d09cf5572c5180bae5737c418.js"
+  "./precache-manifest.3ff1a593c25fc0ae02ecf138d089dcb8.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "uq-wallet"});
@@ -30,7 +30,7 @@ workbox.clientsClaim();
 self.__precacheManifest = [
   {
     "url": "index.html",
-    "revision": "12f1f84b710b45c62315cf6f4f8a7b80"
+    "revision": "edd390f7689d3c867ef4a3b4fe7c036a"
   },
   {
     "url": "precache-manifest.0bcb610967ff8f6256217269d0679f44.js",
@@ -91,6 +91,10 @@ self.__precacheManifest = [
   {
     "url": "precache-manifest.40716cf096b262138b15b79b1cdd4e06.js",
     "revision": "40716cf096b262138b15b79b1cdd4e06"
+  },
+  {
+    "url": "precache-manifest.46ff469d09cf5572c5180bae5737c418.js",
+    "revision": "46ff469d09cf5572c5180bae5737c418"
   },
   {
     "url": "precache-manifest.4724e38036d9b274bfe38ee2b47ff537.js",
@@ -198,7 +202,7 @@ self.__precacheManifest = [
   },
   {
     "url": "service-worker.js",
-    "revision": "47fef45cdaa54f514c853ecc722a52c9"
+    "revision": "bd21ff4880300d2268da6cc8f3aca466"
   }
 ].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
